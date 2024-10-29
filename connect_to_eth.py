aa1699b8e8e77cbb8c99b6137824388848255aed
EmmaZhang2017 <emmazhang2017@163.com>
2024-10-30 00:00:48 +0800

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,7 +26,7 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = Web3(HTTPProvider("https://bsc-dataseed2.bnbchain.org"))
+	w3 = Web3(HTTPProvider("https://data-seed-prebsc-2-s3.bnbchain.org:8545"))
 	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 	# The second section requires you to inject middleware into your w3 object and
