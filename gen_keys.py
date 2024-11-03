c1924d540e14a9188dbb665df1df285c7abb0ffc
EmmaZhang2017 <emmazhang2017@163.com>
2024-11-03 22:46:18 +0800

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -50,6 +50,7 @@ def get_keys(challenge, keyId=0, filename="eth_mnemonic.txt"):
 
     return sig, eth_addr
 
+
 if __name__ == "__main__":
     for i in range(4):
         challenge = os.urandom(64)
